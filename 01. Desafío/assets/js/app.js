03e01df477b9df4200e703a87c19a934c04f507f
Nahuel Gómez <ngomezpf@gmail.com>
2021-09-16 00:12:58 -0300

    Primer desafío

---
+++ 01. Desafío/assets/js/app.js
@@ -0,0 +1,7 @@
+let name = prompt("Ingrese su nombre");
+let surname = prompt("Ingrese su apellido");
+let yearOfBirth = prompt("Ingrese su año de nacimiento");
+let currentYear = prompt("Ingrese el año actual");
+let age = parseInt(currentYear) - parseInt(yearOfBirth);
+
+alert(`¡BIenvenido ${name} ${surname}! Usted tiene ${age} años.`);
