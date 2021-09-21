76db9211f9f87388908443220c9c934ea79aee2d
Nahuel Gómez <ngomezpf@gmail.com>
2021-09-20 22:04:07 -0300

    Se agregó un else if

--- 02. Desafío/assets/js/app.js
+++ 02. Desafío/assets/js/app.js
@@ -1,8 +1,10 @@
 const username = prompt("Ingrese su nombre");
 const userAge = Number(prompt("Ingrese su edad"));
 
-if (userAge >= 18) {
-  alert(`¡Hola ${username}. Tenes ${userAge} años. ¡Podes entrar al bar!`);
+if (userAge >= 18 && userAge <= 44) {
+  alert(`¡Hola ${username}! Tenes ${userAge} años. ¡Podes entrar al bar!`);
+} else if (userAge >= 45) {
+  alert(`¡Hola ${username}! Podes entrar al bar y tenes dos cervezas de regalo.`);
 } else {
   alert(`¡Hola ${username}. Tenes ${userAge} años. ¡No podes entrar al bar!`);
 }
