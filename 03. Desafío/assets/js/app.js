710430ce4d8389f61b6380df471bd9621b6b0f91
Nahuel Gómez <ngomezpf@gmail.com>
2021-09-22 13:17:04 -0300

    Ejercicio del desafío N° 3

--- 03. Desafío/assets/js/app.js
+++ 03. Desafío/assets/js/app.js
@@ -4,15 +4,23 @@ let opcion = prompt("¿Desea ver un producto?").toLowerCase();
 // ARRAY
 const productos = ["Guitarra", "Bajo", "Batería", "Teclado"];
 
-if (opcion == "si")
+if (opcion == "si" && opcion != "" && opcion != NaN) {
   for (let i = 0; i < productos.length; i++) {
     alert(productos[i]);
     let moreProducts = prompt("¿Desea ver otro producto?").toLowerCase();
     if (moreProducts == "no") {
-      alert("Gracias por la visita.");
+      alert("Gracias por tu visita.");
       break;
     }
   }
-else {
-  alert("Gracias por la visita.");
+} else if (parseFloat(opcion)) {
+  alert("Es un numero. Tenes que responder SI o NO. Empecemos de vuelta");
+  window.location.reload();
+} else if (opcion === ' ') {
+  alert("Es un espacio vacio. Empecemos de vuelta");
+  window.location.reload();
+} else {
+  alert('Gracias por visitarnos.')
 }
+
+// Ver como validar caracteres especiales
