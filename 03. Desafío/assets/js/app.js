117a15d82cf9a9f03e2dff01e3df53b0796ef404
Nahuel Gómez <ngomezpf@gmail.com>
2021-09-23 00:11:37 -0300

    Ejercicio final del desafío N° 3

--- 03. Desafío/assets/js/app.js
+++ 03. Desafío/assets/js/app.js
@@ -1,20 +1,26 @@
+// Operación que desea realizar
 var opcion = prompt("¿Querés sumar, restar, multiplicar o dividir? ESCRIBE 'EXIT' PARA SALIR.").toLowerCase();
 
+// Validación para que 'opcion' NO contenga espaciado ni números (no supe como hacer que NO contenga caracteres especiales).
 if (opcion === ' ' || opcion == parseFloat(opcion)) {
   alert('Operación inválida');
   window.location.reload();
 }
 
+// Ciclo while
 while (opcion != "exit") {
 
+  // De vuelta la validación para que 'opcion' NO contenga espaciado ni números porque la validación de la línea 4 queda afuera del ciclo.
   if (opcion === ' ' || opcion == parseFloat(opcion)) {
     alert('Operación inválida');
     window.location.reload();
   }
 
+  // Números a ingresar.
   let numeroUno = parseInt(prompt("Un numero"));
   let numeroDos = parseInt(prompt("Otro numero"));
 
+  // Cuerpo principal de las operaciones
   if (opcion == "sumar") {
     resultado = numeroUno + numeroDos;
     alert(`El resultado de la suma es ${resultado}`);
