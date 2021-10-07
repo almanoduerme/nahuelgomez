e1c1c2c53b82bb943910c82418f7de704daec7c1
Nahuel Gómez <ngomezpf@gmail.com>
2021-10-06 23:24:02 -0300

    Desafío N° 5

---
+++ 05. Desafío (Objetos)/assets/js/app.js
@@ -0,0 +1,29 @@
+class crearUsuario {
+  constructor(nombreUsuario, mailUsuario, passUsuario) {
+    this.nombreCuenta = nombreUsuario;
+    this.mailCuenta = mailUsuario;
+    this.passCuenta = passUsuario;
+  }
+
+  mostrarCuenta = function () {
+    let msg = `Hola ${this.nombreCuenta}. Su cuenta se ha creado correctamente.`;
+    let msg2 = `Su correo electrónico es: ${this.mailCuenta} y su contraseña es ${this.passCuenta}`;
+
+    alert(msg);
+    alert(msg2);
+  };
+}
+
+// Crear la cuenta del usuario
+function crearCuentaUsuario() {
+  alert("Vamos a simular un registro de usuario en la aplicación");
+
+  let nombre = prompt("Introduzca su nombre y apellido");
+  let mail = prompt("Introduzca su correo electrónico");
+  let pass = prompt("Introduzca su contraseña");
+
+  let cuentaUsuario = new crearUsuario(nombre, mail, pass);
+  cuentaUsuario.mostrarCuenta();
+}
+
+crearCuentaUsuario();
